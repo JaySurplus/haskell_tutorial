0458e396cfb9a3ded3a538ab41ce871dec40a371
JaySurplus <znznbest2004@gmail.com>
2016-04-02 02:16:53 -0500

    UPdate

---
+++ ch03/BookStore.hs
@@ -0,0 +1,15 @@
+data BookInfo = Book Int String [String]
+ deriving (Show)
+
+data MagazineInfo = Magazine Int String [String]
+ deriving (Show)
+
+
+data BookReview = BookReview BookInfo CustomerID String
+
+type CustomerID = Int
+type ReviewBody = String
+
+data BetterReview = BetterReview BookInfo CustomerID ReviewBody
+
+myInfo = Book 9780135072455 "Algebra of Programming" ["Richard Bird", "Oege de Moor"]
---
+++ ch03/Bool.hs
@@ -0,0 +1,24 @@
+data BookInfo = Book Int String [String]
+ deriving (Show)
+
+data MagazineInfo = Magazine Int String [String]
+ deriving (Show)
+
+
+data BookReview = BookReview BookInfo CustomerID String
+
+type CustomerID = Int
+type ReviewBody = String
+
+data BetterReview = BetterReview BookInfo CustomerID ReviewBody
+
+
+data Bool = False | True
+type CardHolder = String
+type CardNumber = String
+type Address = [String]
+
+data BillingInfo = CreditCard CardNumber CardHolder Address
+ | CashOnDelivery
+ | Invoice CustomerID
+ deriving (Show)
