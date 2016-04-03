a0864db20efc6343cddeca67a68a37f972d95aa5
JaySurplus <znznbest2004@gmail.com>
2016-04-02 20:06:58 -0500

    UPdate Bool.hs

--- ch03/Bool.hs
+++ ch03/Bool.hs
@@ -22,3 +22,8 @@ data BillingInfo = CreditCard CardNumber CardHolder Address
  | CashOnDelivery
  | Invoice CustomerID
  deriving (Show)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
